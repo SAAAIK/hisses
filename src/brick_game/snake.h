a3b408a2753cc3beadd6bb5e363fc18604e4d139
Hotblack Bones <hotblacb@student.21-school.ru>
2025-01-05 21:47:48 +0300

    add state enum and position struct /todo:snake struct fields etc

---
+++ src/brick_game/snake.h
@@ -0,0 +1,22 @@
+// ifndef 
+
+namespase s21 {
+
+
+typedef enum {
+  Start,
+  Pause,
+  Terminate,
+  Left,
+  Right,
+  Up,
+  Down,
+  Action
+} UserAction_t;
+
+typedef struct {
+    int x;
+    int y;
+} Position_t;
+    
+} //s21 namespace
---
+++ src/brick_game/snake/model.h
@@ -0,0 +1,15 @@
+namespace s21 {
+
+class SnakeModel {
+    public:
+    SnakeModel();
+    ~SnakeModel();
+    // тело модели змейки
+    GameInfoStruct game;
+    Position 
+}
+//конструктор и деструктор 
+
+
+
+} // namespace s21
