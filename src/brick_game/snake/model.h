a83bb2f588e5dd73495618ab0b55f10d04a2ab0d
Samella Object <samellao@student.21-school.ru>
2025-01-08 02:14:39 +0300

    added classes snake, field, apple

--- src/brick_game/snake/model.h
+++ src/brick_game/snake/model.h
@@ -1,17 +1,36 @@
-#include '../shake.h' 
+#include '../snake.h' 
 
 namespace s21 {
 
-class SnakeModel {
-    public:
-    SnakeModel();
-    ~SnakeModel();
-    // тело модели змейки
-    GameInfoStruct game;
+class Snake {
+    private:
+    int _snake_length, _snake_direction;
     Position_t Position;
-}
+
+    public:
+    Snake();
+    ~Snake();
+    // тело модели змейки
+};
 //конструктор и деструктор 
 
+class Field {
+    private:
+    int _field_width, _field_height;
+    GameInfoStruct field;
 
+    public:
+    Field();
+    ~Field();
+};
+
+class Apple {
+    private:
+    int _apple_x, _apple_y;
+
+    public:
+    Apple();
+    ~Apple();
+};
 
 } // namespace s21
