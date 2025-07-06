2925f3790dff99ac1a3aeb6a01d24a68d44e63ff
Hotblack Bones <hotblacb@student.21-school.ru>
2025-07-07 01:08:51 +0300

    Добавил защиту от повторного включения заголовочных файлов

--- src/brick_game/snake/controller.h
+++ src/brick_game/snake/controller.h
@@ -1,3 +1,6 @@
+#ifndef SNAKE_CONTROLLER_H
+#define SNAKE_CONTROLLER_H
+
 #include "interface.h"
 #include "model.h"
 
@@ -11,3 +14,5 @@ GameInfo_t* Controller_getGameState(Controller* controller);
 void GameLoop();
 UserAction_t userInput();
 //void Controller_handleInput(Controller* controller, UserAction_t action);
+
+#endif
--- src/brick_game/snake/interface.h
+++ src/brick_game/snake/interface.h
@@ -1,3 +1,8 @@
+#ifndef SNAKE_INTERFACE_H
+#define SNAKE_INTERFACE_H
+
 #include "controller.h"
 
+typedef struct Controller Controller;
 void renderGame(Controller* controller);
+#endif
